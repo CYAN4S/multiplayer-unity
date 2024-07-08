27a8c1ba0d6b73409c1ea1feece050a9e25c127b
CYAN4S <me@cyan4s.com>
2024-07-09 01:58:06 +0900

    Follow quickstart guide

---
+++ Assets/Scripts/HelloWorldManager.cs
@@ -0,0 +1,63 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class HelloWorldManager : MonoBehaviour
+{
+    private NetworkManager networkManager;
+
+    private void Awake()
+    {
+        networkManager = GetComponent<NetworkManager>();
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+        if (!networkManager.IsClient && !networkManager.IsServer)
+        {
+            StartButtons();
+        }
+        else
+        {
+            StatusLabels();
+
+            SubmitNewPosition();
+        }
+
+        GUILayout.EndArea();
+    }
+
+    private void StartButtons()
+    {
+        if (GUILayout.Button("Host")) networkManager.StartHost();
+        if (GUILayout.Button("Client")) networkManager.StartClient();
+        if (GUILayout.Button("Server")) networkManager.StartServer();
+    }
+
+    private void StatusLabels()
+    {
+        var mode = networkManager.IsHost ? "Host" : networkManager.IsServer ? "Server" : "Client";
+
+        GUILayout.Label("Transport: " +
+                        networkManager.NetworkConfig.NetworkTransport.GetType().Name);
+        GUILayout.Label("Mode: " + mode);
+    }
+
+    private void SubmitNewPosition()
+    {
+        if (GUILayout.Button(networkManager.IsServer ? "Move" : "Request Position Change"))
+        {
+            if (networkManager.IsServer && !networkManager.IsClient)
+            {
+                foreach (ulong uid in networkManager.ConnectedClientsIds)
+                    networkManager.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<HelloWorldPlayer>().Move();
+            }
+            else
+            {
+                var playerObject = networkManager.SpawnManager.GetLocalPlayerObject();
+                var player = playerObject.GetComponent<HelloWorldPlayer>();
+                player.Move();
+            }
+        }
+    }
+}
